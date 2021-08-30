2e9974d9c6d61f554706ded1cfd21557612c6dd8
Miriam Jocelyn <mirismarquez@gmail.com>
2021-08-30 16:55:31 -0500

    Modificaciones de Iphone X+

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/libs/sw-utils.js');
-const STATIC_CACHE    = 'static-v2'
-const DYNAMIC_CACHE   = 'dynamic-v1'
+const STATIC_CACHE    = 'static-v3'
+const DYNAMIC_CACHE   = 'dynamic-v3'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 
@@ -39,6 +39,9 @@ self.addEventListener('activate',e=>{
     const respuesta=caches.keys().then(keys=>{
         keys.forEach(key=>{
             if(key!==STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+            if(key!==DYNAMIC_CACHE && key.includes('dynamic')){
                 return caches.delete(key);
             }
         });
